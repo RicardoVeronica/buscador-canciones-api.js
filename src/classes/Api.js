f56c502a25b512b040dc436d4f6c7260cec8e246
setcain <charlie.bronson00@gmail.com>
2020-12-28 13:52:29 -0600

    agrega mensaje de error si no se encuentra resultado en API

--- src/classes/Api.js
+++ src/classes/Api.js
@@ -11,10 +11,27 @@ class Api {
     fetch(url)
       .then((respuesta) => respuesta.json())
       .then((respuesta) => {
-        const { lyrics } = respuesta;
+        if (respuesta.lyrics) {
+          const { lyrics } = respuesta;
 
-        const divResultado = document.getElementById("resultado");
-        divResultado.textContent = lyrics;
+          const divResultado = document.getElementById("resultado");
+          divResultado.textContent = lyrics;
+
+          const headingResultado = document.querySelector(
+            ".letra-resultado h2"
+          );
+          headingResultado.textContent = `
+          Letra de cancion: ${this.cancion} de ${this.artista}
+        `;
+        } else {
+          const divMensajes = document.getElementById("mensajes");
+          divMensajes.textContent = "No existe, prueba con otra busqueda";
+          divMensajes.classList.add("error");
+
+          setTimeout(() => {
+            divMensajes.remove();
+          }, 2500);
+        }
       });
   }
 }
